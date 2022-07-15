100504a4a2f1d8e0208e85f8229eb6179fc8d915
jomare1188 <jomare1188@gmail.com>
2022-07-15 14:00:08 -0300

    first

--- software/ContFree-NGS.py
+++ software/ContFree-NGS.py
@@ -8,13 +8,15 @@ import argparse
 import os
 from ete3 import NCBITaxa
 from Bio import SeqIO
+import re
 
 # Creating arguments
 parser = argparse.ArgumentParser(prog='ContFree-NGS.py', description='Removing reads from contaminating organisms in Next Generation Sequencing datatasets', add_help=True)
 parser.add_argument('--taxonomy', dest='taxonomy_file', metavar='<taxonomy file>', help='A taxonomy classification file', required=True)
 parser.add_argument('--sequencing_type, --s', dest='sequencing_type', metavar='<p or s>', help='paired-end (p) or single-end (s)', required=True)
-parser.add_argument('--R1', dest='R1_file', metavar='<R1 file>', help='FASTQ file 1', required=True)
-parser.add_argument('--R2', dest='R2_file', metavar='<R2 file>', help='FASTQ file 2', required=False)
+parser.add_argument('--R1', dest='R1_file', metavar='<R1 file>', help='read file 1', required=True)
+parser.add_argument('--R2', dest='R2_file', metavar='<R2 file>', help='read file 2', required=False)
+parser.add_argument('-o', dest='output_path', metavar='<output path>', help='path to output files')
 parser.add_argument('--taxon', dest='taxon', metavar='<Taxon>', type=str, help='Only this taxon and its descendants will be maintained',required=True)
 parser.add_argument('--v', '--version', action='version', version='%(prog)s v1.0')
 
@@ -24,9 +26,13 @@ taxonomy_file = args.taxonomy_file
 sequencing_type = args.sequencing_type
 R1_file = args.R1_file
 R2_file = args.R2_file
+output_path = args.output_path
 taxon = args.taxon
 paired = 0
 single = 0
+
+if not os.path.exists(output_path):
+	os.makedirs(output_path)
 
 if sequencing_type == 'p':
 	paired = 1
@@ -39,21 +45,34 @@ else:
 	print("Exiting. \n")
 	exit()
 
+#index_path = "teste/6_contamination_removal/index/"
+#index_R1_file = os.path.join(index_path, R1_file[:-5] + "index")
+#index_R2_file = os.path.join(index_path, R2_file[:-5] + "index")
+
+index_R1 = SeqIO.index_db(R1_file[:-5] + "index")
+index_R2 = SeqIO.index_db(R2_file[:-5] + "index")
+
+#teste/6_contamination_removal/index/ teste/6_contamination_removal/index/SRR4014641.trimmed.R1.index
+
+'''
 #Checking for indexed file and create it if dont exists	
 def create_indexed_db_for_paired_fastq_files():
-    indexdb_R1_file = R1_file[:-5] + "index"
-    indexdb_R2_file = R2_file[:-5] + "index"
-    print("Using pre-created indexed database for your paired-end reads...")
-    if not os.path.exists(indexdb_R1_file) and not os.path.exists(indexdb_R2_file):
-        print("Creating an indexed database for your paired-end reads, please wait ... \n")
-        R1_index_db = SeqIO.index_db(indexdb_R1_file, R1_file, "fastq")
-        R2_index_db = SeqIO.index_db(indexdb_R2_file, R2_file, "fastq")
+	indexdb_R1_file = R1_file[:-5] + "index"
+	indexdb_R2_file = R2_file[:-5] + "index"
+	if not os.path.exists(indexdb_R1_file) and not os.path.exists(indexdb_R2_file):
+		print("Creating an indexed database for your paired-end reads, please wait ... \n")
+		R1_index_db = SeqIO.index_db(indexdb_R1_file, R1_file, "fastq")
+		R2_index_db = SeqIO.index_db(indexdb_R2_file, R2_file, "fastq")
+	else:
+		print("Working with existing index!")
 
 def create_indexed_db_for_single_fastq_files():
 	indexdb_R1_file = R1_file[:-5] + "index"
 	if not os.path.exists(indexdb_R1_file):
 		print("Creating an indexed database for your single-end reads, please wait ... \n")
 		R1_index_db = SeqIO.index_db(indexdb_R1_file, R1_file, "fastq")
+	else:
+		print("Working with existing index!")
 
 if paired == 1:
 	create_indexed_db_for_paired_fastq_files()
@@ -76,6 +95,7 @@ else:
 		print("ERROR: An exception occurred while trying to open your indexed single-end databse")
 		print("Exiting. \n")
 		exit()
+'''
 
 #Getting NCBI taxonomy database and user taxon
 ncbi = NCBITaxa()
@@ -88,11 +108,20 @@ user_tax_id = list(the_values)[0][0]
 descendants.append(user_tax_id)
 
 #Create output names
+
+# Getting file informations - Pattern in SRR1979658.trimmed_00.kraken
+n = re.search(r"(?<=\_)[0-9]+(?=\.kraken)", taxonomy_file)
+iden = re.search(r"[\w]+(?=\.trimmed)", taxonomy_file)
+
 if paired == 1:
-	filtered_R1 = R1_file[:-5] + "filtered.fastq"
-	filtered_R2 = R2_file[:-5] + "filtered.fastq"
-	unfiltered_R1 = R1_file[:-5] + "unclassified.fastq"
-	unfiltered_R2 = R2_file[:-5] + "unclassified.fastq"
+	filtered_R1 = n.group(0) + "." + iden.group(0) + ".trimmed.filtered.R1.fastq"
+	filtered_R2 = n.group(0) + "." + iden.group(0) + ".trimmed.filtered.R2.fastq"
+	unfiltered_R1 = n.group(0) + "." + iden.group(0) + ".trimmed.unclassified.R1.fastq"
+	unfiltered_R2 = n.group(0) + "." + iden.group(0) + ".trimmed.unclassified.R2.fastq"
+	filtered_R1_output = os.path.join(output_path, filtered_R1)
+	filtered_R2_output = os.path.join(output_path, filtered_R2)
+	unfiltered_R1_output = os.path.join(output_path, unfiltered_R1)
+	unfiltered_R2_output = os.path.join(output_path, unfiltered_R2)
 else: 
         filtered_R1 = R1_file[:-5] + "filtered.fastq"
         unfiltered_R1 = R1_file[:-5] + "unclassified.fastq"
@@ -104,9 +133,8 @@ count_contaminant_sequences = 0
 
 #Filtering files
 if paired == 1:
-	with open(taxonomy_file, "r") as taxonomy_classification_file, open(filtered_R1, "w") as classified_R1, open(filtered_R2, "w") as classified_R2, open(unfiltered_R1, "w") as unclassified_R1, open(unfiltered_R2, "w") as unclassified_R2:
+	with open(taxonomy_file, "r") as taxonomy_classification_file, open(filtered_R1_output, "w") as classified_R1, open(filtered_R2_output, "w") as classified_R2, open(unfiltered_R1_output, "w") as unclassified_R1, open(unfiltered_R2_output, "w") as unclassified_R2:
 		for line in taxonomy_classification_file:
-
 			#Getting IDs
 			R1_sequence_id = line.split()[1] + "/1"
 			R2_sequence_id = line.split()[1] + "/2"
@@ -166,5 +194,4 @@ else:
 		print("-------------------------------------------------")
 		print("{} descendants sequences are in the filtered files".format(taxon))
 		print("Contaminant sequences were discarded")
-		print("Unlabelled sequences are in the unclassified files")
-
+	print("Unlabelled sequences are in the unclassified files")
--- software/create_fastq_indexdb.py
+++ software/create_fastq_indexdb.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-'''                             Description
-                This script creates an indexdb for your FASTQ files
-                This step is necessary to run ContFree-NGS
+'''				Description
+		This script creates a indexdb for FASTQ files!
+		This step is necessary to run remove_contaminants.py
 '''
 
 import argparse
@@ -15,6 +15,7 @@ from Bio import SeqIO
 parser = argparse.ArgumentParser(prog='create_indexdb.py', description='create indexdb for FASTQ files', add_help=True)
 parser.add_argument('-R1', dest='R1_file', metavar='<R1 file>', help='R1 FASTQ file', required=True)
 parser.add_argument('-R2', dest='R2_file', metavar='<R1 file>', help='R2 FASTQ file', required=True)
+parser.add_argument('-o', dest='output_path', metavar='<output path>', help='path to output files')
 parser.add_argument('-v', '--version', action='version', version='%(prog)s v1.0')
 
 # Getting arguments
@@ -22,13 +23,36 @@ parser.add_argument('-v', '--version', action='version', version='%(prog)s v1.0'
 args = parser.parse_args()
 R1 = args.R1_file
 R2 = args.R2_file
+output_path = args.output_path
+cwd = os.getcwd()
 
-# Generate index names
+#print(R1)
+#print(R2)
+#teste/3_trimmed_reads/SRR4014643.trimmed.R1.fastq
+#teste/3_trimmed_reads/SRR4014643.trimmed.R2.fastq
 
-index_R1 = R1[:-5] + "index"
-index_R2 = R2[:-5] + "index"
+if not os.path.exists(output_path):
+	os.makedirs(output_path)
 
-# Create indexdb for large fastq files - This process dramatically decreases the runtime and RAM usage when compared to dictionaries.
+# Generate index names 
 
-record_R1_index_db = SeqIO.index_db(index_R1, R1, "fastq")
-record_R2_index_db = SeqIO.index_db(index_R2, R2, "fastq")
+index_R1 = os.path.basename(R1[:-5] + "index")
+index_R2 = os.path.basename(R2[:-5] + "index")
+#print(index_R1 + "basename + index")
+#print(index_R2 + "basename + index")
+#SRR4014643.trimmed.R1.index
+#SRR4014643.trimmed.R2.index
+
+index_R1_output = os.path.join(output_path, index_R1)
+index_R2_output = os.path.join(output_path, index_R2)
+#print(index_R1_output + "join output + basename + index")
+#print(index_R2_output + "join output + basename + index")
+#teste/6_contamination_removal/index/SRR4014643.trimmed.R1.index
+#teste/6_contamination_removal/index/SRR4014643.trimmed.R2.index
+
+#SeqIO.index_db(idx_name, file, format)
+record_R1_index_db = SeqIO.index_db(index_R1_output, R1, "fastq")
+record_R2_index_db = SeqIO.index_db(index_R2_output, R2, "fastq")
+
+#record_R1_index_db = SeqIO.index_db(index_R1_output, R1, "fastq")
+#record_R2_index_db = SeqIO.index_db(index_R2_output, R2, "fastq")
