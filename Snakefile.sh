3afe5b36936e441e3dc623641b87838bfcc664bb
felipevzps <felipe.vzps@gmail.com>
2024-04-01 01:25:31 -0300

    working job script

--- Snakefile.sh
+++ Snakefile.sh
@@ -10,11 +10,10 @@ module load miniconda3
 module load salmon/1.8.0
 module load Trinity/2.8.5
 module load transrate/1.0.3
-module load BUSCO/5.7.0
 conda activate YAATAP
 
 # run complete YAATAP
-snakemake -p -k --resources load=10 -s Snakefile.v2 --cluster "qsub -q all.q -V -l h={params.server} -cwd -pe smp {threads}" --jobs 10 --jobname "{rulename}.{jobid}"
+snakemake -p -k --resources load=10 -s Snakefile.v2 --rerun-triggers mtime --cluster "qsub -q all.q -V -l h={params.server} -cwd -pe smp {threads}" --jobs 10 --jobname "{rulename}.{jobid}"
 
 # generate DAG
 #snakemake --dag | dot -Tsvg > dag.svg
